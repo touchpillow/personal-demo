2f97c2378037045965ace47522f778be266632a5
touchpillow <1071721873@qq.com>
2020-09-20 01:04:56 +0800

    lletcode

--- 2020/9月/9-19.leetcode.js
+++ 2020/9月/9-19.leetcode.js
@@ -72,27 +72,37 @@ var isPrintable = function (targetGrid) {
       }
     }
   }
-  const keys = [...map.entries()].sort((a, b) => a[0]);
-  for (let i = keys.length - 1; i >= 0; i--) {
-    const cur = map.get(keys[i]);
-    let flag = true;
-    for (let m = cur.xMin; m <= cur.xMax; m++) {
-      if (!flag) break;
-      for (let n = cur.yMin; n <= cur.yMax; n++) {
-        if (targetGrid[m][n] !== 0 && targetGrid[m][n] !== keys[i]) {
-          flag = false;
-          break;
+  let keys = [...map.keys()];
+  let flagv = true;
+  while (keys.length && flagv) {
+    const vaa = [];
+    for (const item of keys) {
+      const cur = map.get(item);
+      let flag = true;
+      for (let m = cur.xMin; m <= cur.xMax; m++) {
+        if (!flag) break;
+        for (let n = cur.yMin; n <= cur.yMax; n++) {
+          if (targetGrid[m][n] !== 0 && targetGrid[m][n] !== item) {
+            flag = false;
+            break;
+          }
+        }
+      }
+      if (!flag) continue;
+      vaa.push(item);
+      for (let m = cur.xMin; m <= cur.xMax; m++) {
+        for (let n = cur.yMin; n <= cur.yMax; n++) {
+          targetGrid[m][n] = 0;
         }
       }
     }
-    if (!flag) continue;
-    for (let m = cur.xMin; m <= cur.xMax; m++) {
-      for (let n = cur.yMin; n <= cur.yMax; n++) {
-        targetGrid[m][n] = 0;
-      }
+    if (!vaa.length) {
+      flagv = false;
+    } else {
+      keys = keys.filter((item) => !vaa.includes(item));
     }
   }
-  console.log(targetGrid);
+
   for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
       if (targetGrid[i][j] !== 0) return false;
@@ -116,11 +126,15 @@ var isPrintable = function (targetGrid) {
 //   [1, 1, 3, 3],
 //   [1, 1, 3, 4],
 //   [5, 5, 1, 4],
-// // ];
+// ];
+// const c = [
+//   [6, 2, 2, 5],
+//   [2, 2, 2, 5],
+//   [2, 2, 2, 5],
+//   [4, 3, 3, 4],
+// ];
 const c = [
-  [6, 2, 2, 5],
-  [2, 2, 2, 5],
-  [2, 2, 2, 5],
-  [4, 3, 3, 4],
+  [1, 1, 1],
+  [3, 1, 3],
 ];
 console.log(isPrintable(c));
