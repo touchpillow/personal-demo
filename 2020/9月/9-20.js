ea1a24be732a26b79a345d8452112243af589cac
touchpillow <1071721873@qq.com>
2020-09-20 20:21:51 +0800

    leetcode

--- 2020/9月/9-20.js
+++ 2020/9月/9-20.js
@@ -26,31 +26,31 @@
 // };
 // const text = "  a";
 // console.log(reorderSpaces(text) == "a  ");
-var maxUniqueSplit = function (s) {
-  const search = (s, set) => {
-    let left = 0;
-    let right = 1;
-    const l = s.length;
-    while (right <= l) {
-      let item = s.slice(left, right);
-      while (set.has(item) && right <= l) {
-        right++;
-        item = s.slice(left, right);
-      }
-      let size = set.size;
-      set.add(item);
-      while (s[right + 1] == s[right]) {
-        size = Math.max(search(s.slice(right, new Set([...set()]))));
-      }
-      left = right;
-      right++;
-    }
-    return set.size;
-  };
-};
-const s = "gpaccacseleac";
+// var maxUniqueSplit = function (s) {
+//   const search = (s, set) => {
+//     let left = 0;
+//     let right = 1;
+//     const l = s.length;
+//     while (right <= l) {
+//       let item = s.slice(left, right);
+//       while (set.has(item) && right <= l) {
+//         right++;
+//         item = s.slice(left, right);
+//       }
+//       let size = set.size;
+//       set.add(item);
+//       while (s[right + 1] == s[right]) {
+//         size = Math.max(search(s.slice(right, new Set([...set()]))));
+//       }
+//       left = right;
+//       right++;
+//     }
+//     return set.size;
+//   };
+// };
+// const s = "gpaccacseleac";
 
-console.log(maxUniqueSplit(s));
+// console.log(maxUniqueSplit(s));
 // var maxProductPath = function (grid) {
 //   const m = Math.pow(10, 9) + 7;
 //   const h = grid.length;
@@ -108,3 +108,91 @@ console.log(maxUniqueSplit(s));
 //   [-1, 4, 0, -3, 1, -3, -1, 2, 1, -3, -1, -4, 4, 1],
 // ];
 // console.log(maxProductPath(grid));
+// var calculate = function (s) {
+//   const va = ["+", "-", "*", "/"];
+//   const res = [];
+//   let temp = "";
+//   for (const n of s) {
+//     if (va.includes(n)) {
+//       res.push(temp);
+//       res.push(n);
+//       temp = "";
+//     } else {
+//       temp += n;
+//     }
+//   }
+//   if (temp) {
+//     res.push(temp);
+//   }
+//   const list = [];
+//   for (let i = 0, l = res.length; i < l; i++) {
+//     if (res[i] == "*" || res[i] == "/") {
+//       const pre = list.pop();
+//       if (res[i] == "*") {
+//         list.push(pre * res[i + 1]);
+//       } else {
+//         list.push(~~(pre / res[i + 1]));
+//       }
+//       i++;
+//     } else {
+//       list.push(res[i]);
+//     }
+//   }
+//   let v = +list[0];
+//   for (let i = 1; i < list.length; i++) {
+//     if (list[i] == "+") {
+//       v += +list[i + 1];
+//       i++;
+//     } else if (list[i] == "-") {
+//       v -= list[i + 1];
+//       i++;
+//     }
+//   }
+//   return v;
+// };
+// console.log(calculate("3+2*2"));
+// const getV = (n) => {
+//   return ((n - 1) * n) / 2;
+// };
+// var numPairsDivisibleBy60 = function (time) {
+//   const map = new Map();
+//   for (const n of time) {
+//     map.set(n % 60, (map.get(n % 60) || 0) + 1);
+//   }
+//   let count = 0;
+//   for (const key of map.keys()) {
+//     if (!key || key == 30) {
+//       count += 2 * getV(map.get(key));
+//     } else {
+//       count += map.get(key) * map.get(60 - key);
+//     }
+//   }
+//   return count / 2;
+// };
+// const a = [60, 60, 60];
+// console.log(numPairsDivisibleBy60(a));
+var canThreePartsEqualSum = function (A) {
+  const l = A.length;
+  for (let i = 1; i < l; i++) {
+    A[i] = A[i - 1] + A[i];
+  }
+  if (A[l - 1] % 3) return false;
+  let left = 0;
+  while (left < l) {
+    if (A[left] == A[l - 1] / 3) {
+      break;
+    }
+    left++;
+  }
+  let right = l - 2;
+  while (right > left) {
+    if (A[right] == (2 * A[l - 1]) / 3) {
+      break;
+    }
+    right--;
+  }
+  return right - left >= 1;
+};
+const a = [1, -1, 1, -1];
+
+console.log(canThreePartsEqualSum(a));
