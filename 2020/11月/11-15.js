e0c23ed49f2b1fbbd130f63a9dd8edb32077fc86
touchpillow <1071721873@qq.com>
2020-11-15 14:04:45 +0800

    leetcode

--- 2020/11月/11-15.js
+++ 2020/11月/11-15.js
@@ -24,17 +24,52 @@
 //   }
 //   return sum;
 // };
-var firstUniqChar = function (s) {
-  if (!s) return " ";
-  const map = {};
-  const res = [];
-  for (const item of s) {
-    if (map[item] !== undefined) {
-      map[item] = false;
+// var firstUniqChar = function (s) {
+//   if (!s) return " ";
+//   const map = {};
+//   const res = [];
+//   for (const item of s) {
+//     if (map[item] !== undefined) {
+//       map[item] = false;
+//     } else {
+//       map[item] = true;
+//     }
+//     res.push(item);
+//   }
+//   return res.find((item) => map[item] === undefined) || " ";
+// };
+// var numberOfArithmeticSlices = function (A) {
+//   let c = 0;
+//   const l = A.length;
+//   const search = (len, pre, dis) => {
+//     if (pre >= l) return;
+//     if (!len) {
+//       for (let i = pre + 1; i < l; i++) {
+//         search(len + 1, i, 0);
+//       }
+//     } else if (len == 1) {
+//       search(len + 1, pre + 1, A[pre + 1] - A[pre]);
+//     } else if (len >= 2 && A[pre + 1] - A[pre] === dis) {
+//       c++;
+//       search(len + 1, pre + 1, dis);
+//     }
+//   };
+//   search(0, -1, 0);
+//   return c;
+// };
+var numberOfArithmeticSlices = function (A) {
+  let c = 0;
+  let sum = 0;
+  const l = A.length;
+  for (let i = 2; i < l; i++) {
+    if (A[i] - A[i - 1] === A[i - 1] - A[i - 2]) {
+      c++;
+      sum += c;
     } else {
-      map[item] = true;
+      c = 0;
     }
-    res.push(item);
   }
-  return res.find((item) => map[item] === undefined) || " ";
+  return sum;
 };
+const a = [1, 2, 3, 4, 5, 6];
+console.log(numberOfArithmeticSlices(a));
