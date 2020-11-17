3def2c74791e509270ab43410c6363f926a31b01
touchpillow <1071721873@qq.com>
2020-11-17 22:25:15 +0800

    leetcode

--- 2020/11月/11-16.js
+++ 2020/11月/11-16.js
@@ -93,30 +93,112 @@
 // const a = [5, 7, 7, 8, 8, 10],
 //   b = 8;
 // console.log(search(a, b));
-var getLeastNumbers = function (arr, k, pre = []) {
-  console.log(arr, k, pre);
-  if (!k) return pre;
-  const l = arr.length;
-  const mid = ~~(l / 2);
-  let left = [];
-  let right = [];
-  for (let i = 0; i < l; i++) {
-    if (i == mid) continue;
-    if (arr[i] <= arr[mid]) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+// var getLeastNumbers = function (arr, k, pre = []) {
+//   console.log(arr, k, pre);
+//   if (!k) return pre;
+//   const l = arr.length;
+//   const mid = ~~(l / 2);
+//   let left = [];
+//   let right = [];
+//   for (let i = 0; i < l; i++) {
+//     if (i == mid) continue;
+//     if (arr[i] <= arr[mid]) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+//   if (k == left.length) return pre.concat(left);
+//   if (k == left.length + 1) return pre.concat(left, arr[mid]);
+//   if (k > left.length)
+//     return getLeastNumbers(
+//       right,
+//       k - left.length - 1,
+//       pre.concat(left, arr[mid])
+//     );
+//   return getLeastNumbers(left, k, pre);
+// };
+// const a = [0, 0, 1, 3, 4, 5, 0, 7, 6, 7];
+// console.log(getLeastNumbers(a, 9));
+// var missingNumber = function (nums) {
+//   let sum = 0;
+//   const l = nums.length;
+//   for (const n of nums) {
+//     sum += n;
+//   }
+//   return ((0 + l - 1) * l) / 2 - sum;
+// };
+// var kthLargest = function (root, k) {
+//   let res;
+//   const search = (root) => {
+//     if (res !== undefined) return;
+//     if (!root) return;
+//     search(root.right);
+//     k--;
+//     if (!k && res === undefined) {
+//       res = root.val;
+//     }
+//     search(root.left);
+//   };
+//   search(root);
+//   return res;
+// };
+// const a = {
+//   b: {
+//     c: 1,
+//   },
+// };
+// const copy = Object.assign({}, a);
+// copy.b === a.b; //true
+var pacificAtlantic = function (matrix) {
+  const res = [];
+  const h = matrix.length;
+  if (!h) return res;
+  const w = matrix[0].length;
+  const dx = new Set();
+  const tp = new Set();
+  for (let i = 0; i < w; i++) {
+    dfs(tp, 0, i);
+  }
+  for (let i = 0; i < h; i++) {
+    dfs(tp, i, 0);
+  }
+  for (let i = 0; i < w; i++) {
+    dfs(dx, h - 1, w - 1 - i);
+  }
+  for (let i = 0; i < h; i++) {
+    dfs(dx, h - 1 - i, w - 1);
+  }
+  const dfs = (set, i, j) => {
+    if (i < 0 || j >= w) return;
+    if (set.has(`${i}-${j}`)) return;
+    set.add(`${i}-${j}`);
+    if (matrix[i]?.[j] <= matrix[i]?.[j - 1]) {
+      dfs(set, i, j - 1);
+    }
+    if (matrix[i]?.[j] <= matrix[i]?.[j + 1]) {
+      dfs(set, i, j + 1);
+    }
+    if (matrix[i]?.[j] <= matrix[i + 1]?.[j]) {
+      dfs(set, i + 1, j);
+    }
+    if (matrix[i]?.[j] <= matrix[i - 1]?.[j]) {
+      dfs(set, i - 1, j);
+    }
+  };
+
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (dx.has(`${i}-${j}`) && tp.has(`${i}-${j}`)) {
+        res.push([i, j]);
+      }
     }
   }
-  if (k == left.length) return pre.concat(left);
-  if (k == left.length + 1) return pre.concat(left, arr[mid]);
-  if (k > left.length)
-    return getLeastNumbers(
-      right,
-      k - left.length - 1,
-      pre.concat(left, arr[mid])
-    );
-  return getLeastNumbers(left, k, pre);
+  return res;
 };
-const a = [0, 0, 1, 3, 4, 5, 0, 7, 6, 7];
-console.log(getLeastNumbers(a, 9));
+const matrix = [
+  [1, 2, 3],
+  [8, 9, 4],
+  [7, 6, 5],
+];
+console.log(pacificAtlantic(matrix));
