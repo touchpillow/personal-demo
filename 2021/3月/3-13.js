eab5ee01b7a74e4a71b4f74b0cd52405f5579f8e
touchpillow <1071721873@qq.com>
2021-03-13 16:06:17 +0800

    leetcode

---
+++ 2021/3月/3-13.js
@@ -0,0 +1,65 @@
+// const getmid = (x1, x2) => {
+//   return [(x1[0] + x2[0]) / 2, (x1[1] + x2[1]) / 2];
+// };
+// const getLength = (x1, x2) => {
+//   return Math.pow(x1[0] - x2[0], 2) + Math.pow(x1[1] - x2[1], 2);
+// };
+// var minAreaFreeRect = function (points) {
+//   const l = points.length;
+//   const map = new Map();
+//   let min = Infinity;
+//   for (let i = 0; i < l - 1; i++) {
+//     for (let j = i + 1; j < l; j++) {
+//       const middle = getmid(points[i], points[j]);
+//       const length = getLength(points[i], points[j]);
+//       if (map.has(`${middle[0]}-${middle[1]}-${length}`)) {
+//         const target = map.get(`${middle[0]}-${middle[1]}-${length}`);
+//         min = Math.min(
+//           min,
+//           ...target.map((item) => {
+//             console.log(item, points[i], points[j], middle);
+//             return getLength(item, points[i]) * getLength(item, points[j]);
+//           })
+//         );
+//         map.get(`${middle[0]}-${middle[1]}-${length}`).push(points[i]);
+//       } else {
+//         map.set(`${middle[0]}-${middle[1]}-${length}`, [points[i]]);
+//       }
+//     }
+//   }
+//   return min === Infinity ? 0 : Math.sqrt(min);
+// };
+// const a = [
+//   [0, 3],
+//   [1, 2],
+//   [3, 1],
+//   [1, 3],
+//   [2, 1],
+// ];
+// console.log(minAreaFreeRect(a));
+const transform = (s) => {
+  return s.toLowerCase().replace(/[aeiou]/g, "*");
+};
+var spellchecker = function (wordlist, queries) {
+  const map2 = new Map();
+  const map3 = new Map();
+  const map = new Map();
+  const l = wordlist.length;
+  for (let i = 0; i < l; i++) {
+    map.set(wordlist[i], i);
+    if (!map2.has(wordlist[i].toLowerCase())) {
+      map2.set(wordlist[i].toLowerCase(), i);
+    }
+    const newWord = transform(wordlist[i]);
+    if (!map3.has(newWord)) {
+      map3.set(newWord, i);
+    }
+  }
+  return queries.map((item) => {
+    return (
+      wordlist[
+        map.get(item) ?? map2.get(item.toLowerCase()) ?? map3.get(newItem)
+      ] ?? ""
+    );
+  });
+};
