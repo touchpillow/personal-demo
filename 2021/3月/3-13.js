6f3670e30ec0485a15b7b18d494c4e8fcc44e689
touchpillow <1071721873@qq.com>
2021-03-14 02:06:24 +0800

    dfs

--- 2021/3月/3-13.js
+++ 2021/3月/3-13.js
@@ -89,3 +89,91 @@
 // console.log(fn("abcd"));
 // console.log(a);
 // let a = "";
+// var increasingBST = function (root) {
+//   if (!root) return root;
+//   root.right = increasingBST(root.right);
+//   if (!root.left) return root;
+//   const left = increasingBST(root.left);
+// //   root.left = null;
+//   let node = left;
+//   while (node.right) {
+//     node = node.right;
+//   }
+//   node.right = root;
+//   return left;
+// };
+//已经得到节点1
+// 1 -> 2 -> 3  -> 4 ->5 -> 6
+//得到4
+// 4 ->5 -> 6
+// ab
+//
+// const fn = (s) => {
+//   s = [...s];
+//   console.log(s);
+//   const dfs = (pre, next, res = []) => {
+//     if (!next.length) return res.concat(pre);
+//     return next.reduce((res, item, i) => {
+//       return dfs(
+//         `${pre}${item}`,
+//         next.slice(0, i).concat(next.slice(i + 1)),
+//         res
+//       );
+//     }, res);
+//   };
+//   return dfs("", s);
+// };
+// String.prototype.reduce = Array.prototype.reduce;
+// const fn = (s, pre = "", res = []) => {
+//   if (!s.length) return res.concat(pre);
+//   return [].reduce.call(
+//     s,
+//     (res, item, i) => {
+//       return fn(`${s.slice(0, i)}${s.slice(i + 1)}`, `${pre}${item}`, res);
+//     },
+//     res
+//   );
+// };
+// console.log(fn("123"));
+const fn = (s) => {
+  const stack = [
+    {
+      pre: "",
+      next: s,
+    },
+  ];
+  const res = [];
+
+  //next有值，说明字符还没取完
+  while (stack.length) {
+    const cur = stack.shift();
+    const next = cur.next;
+    const nextLen = next.length;
+    if (!nextLen) {
+      res.push(cur.pre);
+      continue;
+    }
+    //从剩余字符串中取每一个字符拼接当前字符
+    //下一轮循环用到的字符串要去掉当前的字符
+    for (let i = 0; i < nextLen; i++) {
+      stack.unshift({
+        pre: `${cur.pre}${next[i]}`,
+        next: `${next.slice(0, i)}${next.slice(i + 1)}`,
+      });
+    }
+  }
+  return res;
+};
+console.log(fn("123"));
+// var increasingBST = function (root) {
+//   const head = {};
+//   let pre = head;
+//   const dfs = (root) => {
+//     if (!root) return;
+//     dfs(root.right);
+//     dfs(root.left);
+//     //handle
+//   };
+//   dfs(root);
+//   return head.right;
+// };
