b0ff54d2ba3ec80ea437eebaebdb2821c27b4e2a
touchpillow <1071721873@qq.com>
2021-03-24 01:24:14 +0800

    leetcode

--- 2021/3月/3-23.js
+++ 2021/3月/3-23.js
@@ -22,28 +22,40 @@
 //   }
 //   return init;
 // };
-var minDominoRotations = function (A, B) {
-  const set = new Set();
-  const l = A.length;
-  set.add(A[0]);
-  set.add(B[0]);
-  for (let i = 1; i < l; i++) {
-    if (!set.has(A[i]) && !set.has(B[i])) return -1;
-    for (const item of set) {
-      if (item !== A[i] && item !== B[i]) {
-        set.delete(item);
-      }
-    }
-  }
-  const values = [...set][0];
-  let v1 = 0,
-    v2 = 0;
-  for (let i = 1; i < l; i++) {
-    if (A[i] === values) v1++;
-    if (B[i] === values) v2++;
-  }
-  return Math.min(l - v1, l - v2);
-};
-const A = [2, 1, 2, 4, 2, 2],
-  B = [5, 2, 6, 2, 3, 2];
-console.log(minDominoRotations(A, B));
+// var minDominoRotations = function (A, B) {
+//   const set = new Set();
+//   const l = A.length;
+//   set.add(A[0]);
+//   set.add(B[0]);
+//   for (let i = 1; i < l; i++) {
+//     if (!set.has(A[i]) && !set.has(B[i])) return -1;
+//     for (const item of set) {
+//       if (item !== A[i] && item !== B[i]) {
+//         set.delete(item);
+//       }
+//     }
+//   }
+//   const values = [...set][0];
+//   let v1 = 0,
+//     v2 = 0;
+//   for (let i = 1; i < l; i++) {
+//     if (A[i] === values) v1++;
+//     if (B[i] === values) v2++;
+//   }
+//   return Math.min(l - v1, l - v2);
+// };
+// const A = [2, 1, 2, 4, 2, 2],
+//   B = [5, 2, 6, 2, 3, 2];
+// console.log(minDominoRotations(A, B));
+//  122 n n 4
+// 012345 9 n
+// n +  6 - n = 6 - 1
+// var getMaximumConsecutive = function (coins) {
+//   coins.sort((a, b) => a - b);
+//   let cur = 0;
+//   for (let i = 0; i < coins.length; i++) {
+//     if (coins[i] > cur + 1) break;
+//     cur += coins[i];
+//   }
+//   return cur + 1;
+// };
--- 2021/3月/huahua/03-21.js
+++ 2021/3月/huahua/03-21.js
@@ -6,19 +6,19 @@
 var maxAscendingSum = function (nums) {
   let pre = -Infinity,
     max = 0,
-    sum = 0
+    sum = 0;
   for (var i = 0; i < nums.length; i++) {
-    const cur = nums[i]
+    const cur = nums[i];
     if (cur > pre) {
-      sum += cur
+      sum += cur;
     } else {
-      sum = cur
+      sum = cur;
     }
-    max = Math.max(sum, max)
-    pre = cur
+    max = Math.max(sum, max);
+    pre = cur;
   }
-  return max
-}
+  return max;
+};
 
 // 输入：orders = [[10,5,0],[15,2,1],[25,1,1],[30,4,0]]
 // 输出：6
@@ -36,68 +36,68 @@ var maxAscendingSum = function (nums) {
  */
 var getNumberOfBacklogOrders = function (orders) {
   var buyArr = [],
-    sellArr = []
+    sellArr = [];
 
   for (var i = 0; i < orders.length; i++) {
-    const cur = orders[i]
-    let amount = orders[i][1]
-    var j = 0
+    const cur = orders[i];
+    let amount = orders[i][1];
+    var j = 0;
     if (cur[2]) {
       // sell
       while (amount && j < buyArr.length) {
-        const buy = buyArr[j]
+        const buy = buyArr[j];
         if (buy[0] >= cur[0]) {
           if (buy[1] > amount) {
-            buyArr[j][1] = buy[1] - amount
-            amount = 0
+            buyArr[j][1] = buy[1] - amount;
+            amount = 0;
           } else {
-            amount = amount - buy[1]
-            buyArr[j][1] = 0
+            amount = amount - buy[1];
+            buyArr[j][1] = 0;
           }
           if (amount <= 0) {
-            break
+            break;
           }
         }
-        j++
+        j++;
       }
       if (amount) {
-        let arr = [...orders[i]]
-        arr[1] = amount
-        sellArr.push(arr)
+        let arr = [...orders[i]];
+        arr[1] = amount;
+        sellArr.push(arr);
         sellArr.sort(
           ([pricei, amounti, orderTypei], [pricej, amountj, orderTypej]) => {
-            return pricei - pricej
+            return pricei - pricej;
           }
-        )
+        );
         // console.log(sellArr)
       }
     } else {
       // buy
       while (amount && j < sellArr.length) {
-        const sell = sellArr[j]
+        const sell = sellArr[j];
         if (cur[0] >= sell[0]) {
           if (sell[1] > amount) {
-            sellArr[j][1] = sell[1] - amount
-            amount = 0
+            sellArr[j][1] = sell[1] - amount;
+            amount = 0;
           } else {
-            amount = amount - sell[1]
-            sellArr[j][1] = 0
+            amount = amount - sell[1];
+            sellArr[j][1] = 0;
           }
           if (amount <= 0) {
-            break
+            break;
           }
         }
-        j++
+        j++;
       }
       if (amount) {
-        let arr = [...orders[i]]
-        arr[1] = amount
-        buyArr.push(arr)
+        let arr = [...orders[i]];
+        arr[1] = amount;
+        buyArr.push(arr);
         buyArr.sort(
           ([pricei, amounti, orderTypei], [pricej, amountj, orderTypej]) => {
-            return pricej - pricei
+            return pricej - pricei;
           }
-        )
+        );
         // console.log(buyArr)
       }
     }
@@ -107,102 +107,101 @@ var getNumberOfBacklogOrders = function (orders) {
     (buyArr.reduce((pre, item) => pre + item[1], 0) +
       sellArr.reduce((pre, item) => pre + item[1], 0)) %
     (Math.pow(10, 9) + 7)
-  )
-}
+  );
+};
 
 var getNumberOfBacklogOrders1 = function (orders) {
   var buyArr = [],
-    sellArr = []
+    sellArr = [];
 
   for (var i = 0; i < orders.length; i++) {
-    const cur = orders[i]
+    const cur = orders[i];
     let price = cur[0],
       amount = cur[1],
-      orderType = cur[2]
+      orderType = cur[2];
 
-    var j = 0
     if (orderType) {
       // sell
-      while (amount && j < buyArr.length) {
-        const buy = buyArr[j]
+      while (amount && buyArr.length) {
+        const buy = buyArr[0];
         let buyPrice = buy[0],
-          buyAmount = buy[1]
+          buyAmount = buy[1];
         if (buyPrice >= price) {
           if (buyAmount > amount) {
-            buy[1] = buyAmount - amount
-            amount = 0
+            buy[1] = buyAmount - amount;
+            amount = 0;
           } else {
-            amount = amount - buyAmount
-            buy[1] = 0
+            amount = amount - buyAmount;
+            // buy[1] = 0
+            buyArr.shift();
           }
           if (amount <= 0) {
-            break
+            break;
           }
         }
-        j++
+        j++;
       }
       if (amount) {
         for (var k = 0; k < sellArr.length; k++) {
           let _cur = sellArr[k],
-            _price = _cur[0]
+            _price = _cur[0];
           if (_price >= price) {
-            break
+            break;
           }
         }
         // 升序
-        sellArr.splice(k, 0, [price, amount, orderType])
+        sellArr.splice(k, 0, [price, amount, orderType]);
       }
-      buyArr = buyArr.filter((item) => item[1])
+      // buyArr = buyArr.filter((item) => item[1]);
     } else {
       // buy
       while (amount && j < sellArr.length) {
-        const sell = sellArr[j]
+        const sell = sellArr[j];
         let sellPrice = sell[0],
-          sellAmount = sell[1]
+          sellAmount = sell[1];
 
         if (price >= sellPrice) {
           if (sellAmount > amount) {
-            sell[1] = sellAmount - amount
-            amount = 0
+            sell[1] = sellAmount - amount;
+            amount = 0;
           } else {
-            amount = amount - sellAmount
-            sell[1] = 0
+            amount = amount - sellAmount;
+            sell[1] = 0;
           }
           if (amount <= 0) {
-            break
+            break;
           }
         }
-        j++
+        j++;
       }
       if (amount) {
         for (var k = 0; k < buyArr.length; k++) {
-          let _price = buyArr[k][0]
+          let _price = buyArr[k][0];
           if (_price <= price) {
-            break
+            break;
           }
         }
         // 降序
-        buyArr.splice(k, 0, [price, amount, orderType])
+        buyArr.splice(k, 0, [price, amount, orderType]);
       }
-      sellArr = sellArr.filter((item) => item[1])
+      sellArr = sellArr.filter((item) => item[1]);
     }
   }
-  console.log(buyArr, sellArr)
+  console.log(buyArr, sellArr);
   let sum =
     (buyArr.reduce((pre, item) => pre + item[1], 0) +
       sellArr.reduce((pre, item) => pre + item[1], 0)) %
-    (Math.pow(10, 9) + 7)
-  console.log(sum)
-  return sum
-}
+    (Math.pow(10, 9) + 7);
+  console.log(sum);
+  return sum;
+};
 
 getNumberOfBacklogOrders1([
   [10, 5, 0],
   [15, 2, 1],
   [25, 1, 1],
   [30, 4, 0],
-])
-
+]);
 
 /**
  * 5711. 有界数组中指定下标处的最大值
@@ -211,6 +210,90 @@ getNumberOfBacklogOrders1([
  * @param {number} maxSum
  * @return {number}
  */
-var maxValue = function(n, index, maxSum) {
-
+var maxValue = function (n, index, maxSum) {};
+// var getNumberOfBacklogOrders = function (orders) {
+//   const list0 = [];
+//   const list1 = [];
+//   for (const item of orders) {
+//     if (item[2] === 0) {
+//       //   if (!list1.length) {
+//       //     list0.push(item);
+//       //     continue;
+//       //   }
+//       while (list1.length && item[1] && list1[0][0] <= item[0]) {
+//         if (list1[0][1] <= item[1]) {
+//           item[1] -= list1[0][1];
+//           list1.shift();
+//         } else {
+//           list1[0][1] -= item[1];
+//           item[1] = 0;
+//         }
+//       }
+//       if (item[1] > 0) {
+//         const index = list0.findIndex((i) => i[0] >= item[0]);
+//         if (index < 0) {
+//           list0.push(item);
+//         } else {
+//           list0.splice(index, 0, item);
+//         }
+//       }
+//     } else {
+//       while (list0.length && item[1] && list0[list0.length - 1][0] >= item[0]) {
+//         if (list0[list0.length - 1][1] <= item[1]) {
+//           item[1] -= list0[list0.length - 1][1];
+//           list0.pop();
+//         } else {
+//           list0[list0.length - 1][1] -= item[1];
+//           item[1] = 0;
+//         }
+//       }
+//       if (item[1] > 0) {
+//         const index = list1.findIndex((i) => i[0] >= item[0]);
+//         if (index < 0) {
+//           list1.push(item);
+//         } else {
+//           list1.splice(index, 0, item);
+//         }
+//       }
+//     }
+//   }
+//   const mod = Math.pow(10, 9) + 7;
+//   return [...list0, ...list1].reduce((pre, item) => {
+//     return (pre + item[1]) % mod;
+//   }, 0); n 1- maxSum)*n -n
+// };  n-1   n-1 n-2     index =2
+//      1 ...n-2 n-1 3 4 5  l-1
+var maxValue = function (n, index, maxSum) {
+  const realV = maxSum;
+  // 因为后续计算是从0开始，所以先减去n
+  maxSum -= n;
+  //两个等差数列求和
+  const getSum = (max) => {
+    //left 是左侧数列和
+    //right 是右侧数列和 从0-max的和
+    const left = Math.min(index + 1, max);
+    const right = Math.min(n - 1 - index, max);
+    return (
+      (left * (2 * max - left + 1)) / 2 +
+      (right * (2 * max - 2 - right + 1)) / 2
+    );
+  };
+  //二分法求得
+  let left = 0;
+  let right = realV;
+  while (right > left) {
+    if (right - left === 1) break;
+    const mid = ~~((left + right) / 2);
+    if (mid === left) break;
+    const v = getSum(mid);
+    if (v === maxSum) return mid + 1;
+    if (v > maxSum) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+  //前面每个位置减了1，这里加上
+  if (getSum(right) <= maxSum) return right + 1;
+  return left + 1;
 };
