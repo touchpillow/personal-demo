8b6ed914bcbd08455f3a64d2be99d4bc4d7455be
touchpillow <1071721873@qq.com>
2021-05-02 11:43:25 +0800

    leetcode

--- 2021/5月/5-1.leetcode.js
+++ 2021/5月/5-1.leetcode.js
@@ -12,10 +12,7 @@
 //   return res;
 // };
 // var SeatManager = function (n) {
-//   this.list = new Array(n);
-//   for (let i = 1; i <= n; i++) {
-//     this.list[i - 1] = i;
-//   }
+//   this.list = [];
 // };
 
 // /**
@@ -31,12 +28,28 @@
 //  */
 
 // SeatManager.prototype.unreserve = function (seatNumber) {
-//   const index = this.list.findIndex((i) => i > n);
-//   if (index < 0) {
-//     this.list.push(n);
-//   } else {
-//     this.list.splice(index, 0, n);
+//   let left = 0;
+//   let right = this.list.length - 1;
+//   if (seatNumber < this.list[left]) {
+//     this.list.unshift(seatNumber);
+//     return;
+//   } else if (seatNumber > this.list[right]) {
+//     this.list.push(seatNumber);
+//     return;
 //   }
+//   while (right >= left) {
+//     if (left === right) {
+//       break;
+//     }
+//     const mid = ~~((left + right) / 2);
+//     if (mid === left) break;
+//     if (this.list[mid] > seatNumber) {
+//       right = mid;
+//     } else {
+//       left = mid;
+//     }
+//   }
+//   this.list.splice(right, 0, seatNumber);
 // };
 // var maximumElementAfterDecrementingAndRearranging = function (arr) {
 //   arr.sort((a, b) => a - b);
@@ -53,48 +66,104 @@
 // };
 // const a = [100, 1, 1000];
 // console.log(maximumElementAfterDecrementingAndRearranging(a));
-function middle(arr, target) {
+
+function middle(arr, n) {
   let left = 0;
   let right = arr.length - 1;
-  if (arr[left][0]) if (left >= right) return arr[left][0];
-  while (right > left) {}
+  if (arr[left] >= n) return arr[left];
+  if (arr[right] <= n) return arr[right];
+  while (right >= left) {
+    if (left === right) break;
+    const mid = ~~((left + right) / 2);
+    if (mid === left) break;
+    if (arr[mid] === n) return n;
+    if (arr[mid] > n) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+  if (Math.abs(arr[right] - n) < Math.abs(arr[left] - n)) {
+    return arr[right];
+  } else {
+    return arr[left];
+  }
+}
+function insert(arr, n) {
+  if (!arr.length) {
+    arr.push(n);
+  } else {
+    let left = 0;
+    let right = arr.length - 1;
+    if (arr[left] > n) {
+      arr.unshift(n);
+      return;
+    } else if (arr[right] < n) {
+      arr.push(n);
+      return;
+    }
+    while (right >= left) {
+      if (left === right) break;
+      const mid = ~~((left + right) / 2);
+      if (left === mid) break;
+      if (arr[mid] > n) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+    arr.splice(right, 0, n);
+  }
 }
 var closestRoom = function (rooms, queries) {
-  const map = new Map();
-  for (const [id, s] of rooms) {
-    if (!map.has(s)) {
-      map.set(s, []);
-    }
-    map.get(s).push(id);
-  }
-  for (const s of map.keys()) {
-    map.get(s).sort((a, b) => a - b);
-  }
   const k = queries.length;
   const res = new Array(k);
   queries = queries.map((item, i) => [item[0], item[1], i]);
-  queries.sort((a, b) => a[1] - b[1]);
+  queries.sort((a, b) => b[1] - a[1]);
   rooms.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    return a[1] - b[1];
+    return b[1] - a[1];
   });
+  const ids = [];
   for (let i = 0; i < k; i++) {
     const [id, s, index] = queries[i];
-    if (!rooms.length) {
+    while (rooms.length && rooms[0][1] >= s) {
+      const newId = rooms.shift()[0];
+      insert(ids, newId);
+    }
+    if (!ids.length) {
       res[index] = -1;
       continue;
-    }
-    while (rooms.length && rooms[0][1] < s) {
-      rooms.shift();
-    }
-    if (!rooms.length) {
-      res[index] = -1;
-      continue;
-    }
-    if (map.has(s)) {
-      res[index] = map.get(s)[0];
     } else {
-      res[index] = middle(rooms, id);
+      if (index === 0) {
+        console.log(ids);
+      }
+      res[index] = middle(ids, id);
     }
   }
+  return res;
 };
+const a = [
+    [23, 22],
+    [6, 20],
+    [15, 6],
+    [22, 19],
+    [2, 10],
+    [21, 4],
+    [10, 18],
+    [16, 1],
+    [12, 7],
+    [5, 22],
+  ],
+  b = [
+    [12, 5],
+    [15, 15],
+    [21, 6],
+    [15, 1],
+    [23, 4],
+    [15, 11],
+    [1, 24],
+    [3, 19],
+    [25, 8],
+    [18, 6],
+  ];
+console.log(closestRoom(a, b));
---
+++ 2021/5月/5-2.leetcode.js
@@ -0,0 +1,147 @@
+// var getMinDistance = function (nums, target, start) {
+//   const l = nums.length;
+//   let min = l;
+
+//   for (let i = 0; i < l; i++) {
+//     if (nums[i] === target && Math.abs(i - start) < min) {
+//       min = Math.abs(i - start);
+//     }
+//   }
+//   return min;
+// };
+// const nums = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//   target = 1,
+//   start = 0;
+// console.log(getMinDistance(nums, target, start));
+// var splitString = function (s, pre) {
+//   const l = s.length;
+//   const dp = new Array(l).fill("").map(() => new Array(l).fill(""));
+//   for (let i = 0; i < l; i++) {
+//     for (let j = i; j < l; j++) {
+//       dp[i][j] = Number(s.slice(i, j + 1));
+//     }
+//   }
+//   let flag = false;
+//   const dfs = (index, pre = []) => {
+//     if (flag) return;
+//     if (index < 0 && pre.length > 1) {
+//       flag = true;
+//       return;
+//     }
+//     for (let i = index; i >= 0; i--) {
+//       const head = dp[i][index];
+//       if (pre.length && head !== pre[0] + 1) continue;
+//       const target = pre.slice();
+//       target.unshift(dp[i][index]);
+//       dfs(i - 1, target);
+//     }
+//   };
+//   dfs(l - 1);
+//   return flag;
+// };
+// const s = "10009998";
+// console.log(splitString(s));
+function insert(arr, n) {
+  if (!arr.length) {
+    arr.push(n);
+  } else {
+    let left = 0;
+    let right = arr.length - 1;
+    if (arr[left] >= n) {
+      arr.unshift(n);
+      return;
+    } else if (arr[right] <= n) {
+      arr.push(n);
+      return;
+    }
+    while (right >= left) {
+      if (left === right) break;
+      const mid = ~~((left + right) / 2);
+      if (left === mid) break;
+      if (arr[mid] === n) {
+        right = mid;
+        break;
+      }
+      if (arr[mid] > n) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+    arr.splice(right, 0, n);
+  }
+}
+function deleteItem(list, n) {
+  const l = list.length;
+  if (!l) return;
+  let left = 0,
+    right = l - 1;
+  if (list[left] === n) {
+    list.splice(left, 1);
+    return;
+  } else if (list[right] === n) {
+    list.splice(right, 1);
+    return;
+  }
+  while (right > left) {
+    if (left >= right) break;
+    const mid = ~~((left + right) / 2);
+    if (mid === left) break;
+    if (list[mid] === n) {
+      left = mid;
+      break;
+    }
+    if (list[mid] > n) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+  if (list[left] === n) {
+    list.splice(left, 1);
+  }
+}
+var minInterval = function (intervals, queries) {
+  const lenList = [];
+  const startValue = intervals.slice().sort((a, b) => {
+    return a[0] - b[0];
+  });
+  const endValue = intervals.slice().sort((a, b) => {
+    return a[1] - b[1];
+  });
+  const k = queries.length;
+  queries = queries.map((a, i) => [a, i]);
+  queries.sort((a, b) => a[0] - b[0]);
+  const res = new Array(k);
+  for (let i = 0; i < k; i++) {
+    const [v, index] = queries[i];
+    let prev = Infinity;
+    if (i > 0) {
+      prev = queries[i - 1][0];
+    }
+    while (startValue.length && startValue[0][0] <= v) {
+      const item = startValue.shift();
+      if (item[1] < v) continue;
+      insert(lenList, item[1] - item[0] + 1);
+    }
+    while (endValue.length && endValue[0][1] <= v - 1) {
+      const item = endValue.shift();
+      if (item[0] > prev) continue;
+      deleteItem(lenList, item[1] - item[0] + 1);
+    }
+    if (!lenList.length) {
+      res[index] = -1;
+    } else {
+      res[index] = lenList[0];
+    }
+  }
+  return res;
+};
+const intervals = [
+    [2, 3],
+    [2, 5],
+    [1, 8],
+    [20, 25],
+  ],
+  queries = [2, 19, 5, 22];
+console.log(minInterval(intervals, queries));
