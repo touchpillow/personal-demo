74e0e4aa0d201fabe9d51f9aea0883bd66987924
Guofeng Ye <guofeng.ye@shopee.com>
2021-10-21 10:42:02 +0800

    merge

--- 2021/10月/10-18.js
+++ 2021/10月/10-18.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * @Author: your name
  * @Date: 2021-10-18 12:58:35
@@ -40,32 +39,4 @@ var hasValidPath = function (grid) {
     const current = grid[i][j];
     const [x, y] = getNext(i, j);
   };
-=======
-var numTeams = function (rating) {
-  const l = rating.length;
-  let res = 0;
-  for (let i = 1; i < l - 1; i++) {
-    let a = 0,
-      b = 0;
-    let c = 0,
-      d = 0;
-    for (let j = 0; j < i; j++) {
-      if (rating[j] > rating[i]) {
-        a++;
-      } else if (rating[j] < rating[i]) {
-        b++;
-      }
-    }
-    for (let k = i + 1; k < l; k++) {
-      if (rating[k] > rating[i]) {
-        c++;
-      } else if (rating[k] < rating[i]) {
-        d++;
-      }
-    }
-    res += a * d;
-    res += b * c;
-  }
-  return res;
->>>>>>> fc15f17276199fe6ee28a8bf2c1aadc5fe976483
 };
