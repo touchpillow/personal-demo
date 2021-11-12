34a4060459169285e1a20f07de304a212c915627
touchpillow <1071721873@qq.com>
2021-11-12 21:11:39 +0800

    leetcode

---
+++ 2021/11月/11-12.js
@@ -0,0 +1,23 @@
+var pseudoPalindromicPaths = function (root) {
+  const num = new Array(10).fill(0);
+  let c = 0;
+  const dfs = (root) => {
+    if (!root) return;
+    num[root.val]++;
+    if (!root.left && !root.right) {
+      const l = num.filter((i) => i).length;
+
+      if (l < 2) c++;
+    }
+    dfs(root.left);
+    num[root.val]--;
+    dfs(root.right);
+  };
+  return c;
+};
+[2, 1, 1, 1, 3, null, null, null, null, null, 1];
+211311
+   2
+ 1    1
+1  3    
+     1 
