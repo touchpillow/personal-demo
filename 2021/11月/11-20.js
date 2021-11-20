96d36ff0d98792bf974eb9b2184c486d01553022
touchpillow <1071721873@qq.com>
2021-11-20 12:06:34 +0800

    leetcode

---
+++ 2021/11月/11-20.js
@@ -0,0 +1,425 @@
+// const list = [
+//   {
+//     id: 5,
+//     name: "测试类目1",
+//     description: null,
+//     parentId: 2,
+//     sort: 4,
+//     tagType: "category",
+//     configTagList: [
+//       {
+//         id: 6,
+//         name: "测试产品线1",
+//         description: null,
+//         parentId: 5,
+//         sort: 5,
+//         tagType: "product",
+//         configTagList: [
+//           {
+//             id: 131,
+//             name: "2",
+//             description: null,
+//             parentId: 6,
+//             sort: 129,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 132,
+//             name: "3",
+//             description: null,
+//             parentId: 6,
+//             sort: 130,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 133,
+//             name: "4",
+//             description: null,
+//             parentId: 6,
+//             sort: 131,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 134,
+//             name: "5",
+//             description: null,
+//             parentId: 6,
+//             sort: 132,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 135,
+//             name: "6",
+//             description: null,
+//             parentId: 6,
+//             sort: 133,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 136,
+//             name: "78",
+//             description: null,
+//             parentId: 6,
+//             sort: 134,
+//             tagType: "brand",
+//           },
+//         ],
+//       },
+//       {
+//         id: 125,
+//         name: "12321",
+//         description: null,
+//         parentId: 5,
+//         sort: 123,
+//         tagType: "product",
+//         configTagList: [
+//           {
+//             id: 126,
+//             name: "123213",
+//             description: null,
+//             parentId: 125,
+//             sort: 124,
+//             tagType: "brand",
+//             configTagList: [
+//               {
+//                 id: 137,
+//                 name: "1",
+//                 description: null,
+//                 parentId: 126,
+//                 sort: 135,
+//                 tagType: "model",
+//               },
+//               {
+//                 id: 138,
+//                 name: "2",
+//                 description: null,
+//                 parentId: 126,
+//                 sort: 136,
+//                 tagType: "model",
+//               },
+//               {
+//                 id: 139,
+//                 name: "3",
+//                 description: null,
+//                 parentId: 126,
+//                 sort: 137,
+//                 tagType: "model",
+//               },
+//               {
+//                 id: 140,
+//                 name: "34",
+//                 description: null,
+//                 parentId: 126,
+//                 sort: 138,
+//                 tagType: "model",
+//               },
+//             ],
+//           },
+//           {
+//             id: 127,
+//             name: "32432",
+//             description: null,
+//             parentId: 125,
+//             sort: 125,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 128,
+//             name: "12",
+//             description: null,
+//             parentId: 125,
+//             sort: 126,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 129,
+//             name: "3",
+//             description: null,
+//             parentId: 125,
+//             sort: 127,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 130,
+//             name: "1",
+//             description: null,
+//             parentId: 125,
+//             sort: 128,
+//             tagType: "brand",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     id: 21,
+//     name: "测试类目2",
+//     description: null,
+//     parentId: 2,
+//     sort: 19,
+//     tagType: "category",
+//     configTagList: [
+//       {
+//         id: 22,
+//         name: "测试产品线1",
+//         description: null,
+//         parentId: 21,
+//         sort: 20,
+//         tagType: "product",
+//         configTagList: [
+//           {
+//             id: 23,
+//             name: "测试品牌1",
+//             description: null,
+//             parentId: 22,
+//             sort: 21,
+//             tagType: "brand",
+//             configTagList: [
+//               {
+//                 id: 24,
+//                 name: "测试型号1",
+//                 description: null,
+//                 parentId: 23,
+//                 sort: 22,
+//                 tagType: "model",
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     id: 5,
+//     name: "测试类目1",
+//     description: null,
+//     parentId: 2,
+//     sort: 4,
+//     tagType: "category",
+//     configTagList: [
+//       {
+//         id: 6,
+//         name: "测试产品线1",
+//         description: null,
+//         parentId: 5,
+//         sort: 5,
+//         tagType: "product",
+//         configTagList: [
+//           {
+//             id: 131,
+//             name: "2",
+//             description: null,
+//             parentId: 6,
+//             sort: 129,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 132,
+//             name: "3",
+//             description: null,
+//             parentId: 6,
+//             sort: 130,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 133,
+//             name: "4",
+//             description: null,
+//             parentId: 6,
+//             sort: 131,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 134,
+//             name: "5",
+//             description: null,
+//             parentId: 6,
+//             sort: 132,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 135,
+//             name: "6",
+//             description: null,
+//             parentId: 6,
+//             sort: 133,
+//             tagType: "brand",
+//           },
+//           {
+//             id: 136,
+//             name: "78",
+//             description: null,
+//             parentId: 6,
+//             sort: 134,
+//             tagType: "brand",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     id: 21,
+//     name: "测试类目2",
+//     description: null,
+//     parentId: 2,
+//     sort: 19,
+//     tagType: "category",
+//     configTagList: [
+//       {
+//         id: 22,
+//         name: "测试产品线1",
+//         description: null,
+//         parentId: 21,
+//         sort: 20,
+//         tagType: "product",
+//         configTagList: [
+//           {
+//             id: 23,
+//             name: "测试品牌1",
+//             description: null,
+//             parentId: 22,
+//             sort: 21,
+//             tagType: "brand",
+//             configTagList: [
+//               {
+//                 id: 24,
+//                 name: "测试型号1",
+//                 description: null,
+//                 parentId: 23,
+//                 sort: 22,
+//                 tagType: "model",
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
+// const res = [
+//   {
+//     name: "测试类目1/测试产品线1/2",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/3",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/4",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/5",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/6",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/78",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/12321/123213/1",
+//     parentId: "2/5/125/126",
+//     tagType: "category/product/brand/model",
+//   },
+//   {
+//     name: "测试类目1/12321/123213/2",
+//     parentId: "2/5/125/126",
+//     tagType: "category/product/brand/model",
+//   },
+//   {
+//     name: "测试类目1/12321/123213/3",
+//     parentId: "2/5/125/126",
+//     tagType: "category/product/brand/model",
+//   },
+//   {
+//     name: "测试类目1/12321/123213/34",
+//     parentId: "2/5/125/126",
+//     tagType: "category/product/brand/model",
+//   },
+//   {
+//     name: "测试类目1/12321/32432",
+//     parentId: "2/5/125",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/12321/12",
+//     parentId: "2/5/125",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/12321/3",
+//     parentId: "2/5/125",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/12321/1",
+//     parentId: "2/5/125",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目2/测试产品线1/测试品牌1/测试型号1",
+//     parentId: "2/21/22/23",
+//     tagType: "category/product/brand/model",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/2",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/3",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/4",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/5",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/6",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目1/测试产品线1/78",
+//     parentId: "2/5/6",
+//     tagType: "category/product/brand",
+//   },
+//   {
+//     name: "测试类目2/测试产品线1/测试品牌1/测试型号1",
+//     parentId: "2/21/22/23",
+//     tagType: "category/product/brand/model",
+//   },
+// ];
+var findLeastNumOfUniqueInts = function (arr, k) {
+  const map = new Map();
+  for (const n of arr) {
+    map.set(n, (map.get(n) || 0) + 1);
+  }
+  const list = [...map.values()].sort((a, b) => a - b);
+  const l = list.length;
+  let sum = 0;
+  for (let i = 0; i < l; i++) {
+    sum += list[i];
+    if (sum > k) return l - i;
+  }
+  return 0;
+};
