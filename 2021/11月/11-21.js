2fa8ead8d9ef3bcc9789420a8defd4709fcdea2f
touchpillow <1071721873@qq.com>
2021-11-22 22:17:45 +0800

    leetcode

--- 2021/11月/11-21.js
+++ 2021/11月/11-21.js
@@ -7,20 +7,43 @@
 //   }
 //   return -1;
 // };
-var kthFactor = function (n, k) {
-  const list = [];
-  const mid = Math.sqrt(n);
-  for (let i = 1; i <= mid; i++) {
-    if (!(n % i)) {
-      list.push(i);
+// var kthFactor = function (n, k) {
+//   const list = [];
+//   const mid = Math.sqrt(n);
+//   for (let i = 1; i <= mid; i++) {
+//     if (!(n % i)) {
+//       list.push(i);
+//     }
+//   }
+//   const isOdd = !Number.isInteger(mid);
+//   const sum = 2 * list.length - (isOdd ? 0 : 1);
+//   console.log(list, isOdd);
+//   if (k <= list.length) return list[k - 1];
+//   if (k > sum) return -1;
+//   console.log();
+//   return n / list[list.length - 1 - (k - list.length - (isOdd ? 1 : 0))];
+// };
+// console.log(kthFactor(70, 7));
+const getMinCandys = (scores) => {
+  const len = scores.length;
+  const candys = new Array(len);
+  candys[0] = 1;
+  let min = 1;
+  let sum = 1;
+  for (let i = 1; i < len; i++) {
+    if (scores[i] > scores[i - 1]) {
+      candys[i] = candys[i - 1] + 1;
+    } else if (scores[i] === scores[i - 1]) {
+      // 如果同分同糖果，改一下这里
+      candys[i] = candys[i - 1] < 1 ? candys[i - 1] : 1;
+    } else {
+      candys[i] = Math.min(candys[i - 1] - 1, 1);
     }
+    min = Math.min(min, candys[i]);
+
+    sum += candys[i];
   }
-  const isOdd = !Number.isInteger(mid);
-  const sum = 2 * list.length - (isOdd ? 0 : 1);
-  console.log(list, isOdd);
-  if (k <= list.length) return list[k - 1];
-  if (k > sum) return -1;
-  console.log();
-  return n / list[list.length - 1 - (k - list.length - (isOdd ? 1 : 0))];
+  console.log(candys);
+  return sum + (1 - min) * len;
 };
-console.log(kthFactor(70, 7));
+console.log(getMinCandys([5, 4, 4, 1, 1]));
