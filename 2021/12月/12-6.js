66eda58653cab549cd4f8efe52f5baee491048de
touchpillow <1071721873@qq.com>
2021-12-06 22:02:28 +0800

    leetcode

---
+++ 2021/12月/12-6.js
@@ -0,0 +1,8 @@
+var minSwaps = function (grid) {
+  const h = grid.length;
+  const w = grid[0].length;
+  const sizes = new Array();
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {}
+  }
+};
